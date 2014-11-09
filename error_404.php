ba0b5dfa8abd98102c0d54f552d0b24389bb103a
sberry <simon_berry2003@yahoo.com>
2014-11-09 22:29:14 +1030

    Added error page

---
+++ error_404.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html>
+<head>
+<?php include"head.html" ?>
+<body>
+<div id="container">
+    <?php include"menu.html" ?>
+	<div id="body">
+        <div class="textboxtop2">Error 404 - Page not found</div>
+        <div class="textbox">
+            This might be because:
+            <li>You have typed the web address incorrectly, or
+            <li>the page you were looking for may have been moved, updated or deleted.
+            <p>To return to the main site use the links above.
+        </div>
+        <div class="textboxbottom"></div>
+	</div>
+</div>
+</body>
+</html>
--- home.php
+++ home.php
@@ -1,6 +1,5 @@
 <!DOCTYPE html>
 <html>
-<head>
 <?php include"head.html" ?>
 <body>
 <div id="container">
