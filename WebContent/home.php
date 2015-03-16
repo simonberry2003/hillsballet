32e48f5b0670f431b0c0a80012ffaa57f7d9f42b
sberry <simon_berry2003@yahoo.com>
2015-03-16 23:07:12 +1030

    Removed title for french translation services

--- WebContent/home.php
+++ WebContent/home.php
@@ -4,7 +4,6 @@
 <meta charset="utf-8">
 <meta http-equiv="X-UA-Compatible" content="IE=edge">
 <meta name="viewport" content="width=device-width, initial-scale=1">
-<title>French Translation Services</title>
 <link href="css/bootstrap.min.css" rel="stylesheet">
 <?php include "includes.php" ?>
 <?php include "head.html" ?>
