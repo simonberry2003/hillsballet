8fc941ff317dbdf4b92e4d395128eb32e78e01d6
sberry <simon_berry2003@yahoo.com>
2015-03-24 20:23:45 +1030

    updated timetable

--- WebContent/timetable.php
+++ WebContent/timetable.php
@@ -23,45 +23,61 @@
 					<div class="panel-body">
 						<table class="table table-hover">
 							<tr>
-								<th>Time</th>
-								<th>Class</th>
+								<th class="col-md-2">Time</th>
+								<th class="col-md-3">Class</th>
+                                <th>Venue</th>
 							</tr>
 							<tr>
-								<td>9.00 - 9.45</td>
-								<td>Pre-Primary/Primary</td>
+								<td class="col-md-2">9.00 - 9.45</td>
+								<td class="col-md-4">Pre-Primary/Primary</td>
+                                <td class="col-md-3">Mount Lofty Scout Hut</td>
 							</tr>
 							<tr>
-								<td>9.45 - 10.30</td>
-								<td>Grade 1/2</td>
+								<td class="col-md-2">9.45 - 10.30</td>
+								<td class="col-md-4">Grade 1/2</td>
+                                <td class="col-md-3">Mount Lofty Scout Hut</td>
 							</tr>
 							<tr>
-								<td>10.30 - 11.10</td>
-								<td>Little Tiptoes</td>
+								<td class="col-md-2">10.30 - 11.10</td>
+								<td class="col-md-4">Little Tiptoes</td>
+                                <td class="col-md-3">Mount Lofty Scout Hut</td>
 							</tr>
 							<tr>
-								<td>11.15 - 11.55</td>
-								<td>Leap into Dance</td>
+								<td class="col-md-2">11.15 - 11.55</td>
+								<td class="col-md-4">Leap into Dance</td>
+                                <td class="col-md-3">Mount Lofty Scout Hut</td>
 							</tr>
 						</table>
 					</div>
-				</div>
-				<div class="panel panel-default">
 					<div class="panel-heading">
 						<h3 class="panel-title">Thursday Timetable</h3>
 					</div>
 					<div class="panel-body">
 						<table class="table table-hover">
 							<tr>
-								<th>Time</th>
-								<th>Class</th>
+								<th class="col-md-2">Time</th>
+								<th class="col-md-4">Class</th>
+                                <th class="col-md-3">Venue</th>
+							</tr>
+                            <tr>
+                                <td class="col-md-2">9.15 - 9.55</td>
+                                <td class="col-md-4">Leap Into Dance <B>**new**</B></td>
+                                <td class="col-md-3">Mount Lofty Scout Hut</td>
+                            </tr>
+                            <tr>
+                                <td class="col-md-2">10.00 - 10.40</td>
+                                <td class="col-md-4">Little Tiptoes <B>**new**</B></td>
+                                <td class="col-md-3">Mount Lofty Scout Hut</td>
+                            </tr>
+							<tr>
+								<td class="col-md-2">4.00 - 4.45</td>
+								<td class="col-md-4">Grade 2 Exam prep/Jazz/Tap/Performance</td>
+                                <td class="col-md-3">Aldgate Oval Hall</td>
 							</tr>
 							<tr>
-								<td>4.00 – 4.45</td>
-								<td>Grade 2 Exam prep/Jazz/Tap/Performance</td>
-							</tr>
-							<tr>
-								<td>4.45 – 5.30</td>
-								<td>Pre-primary/Primary including Primary Exam Prep</td>
+								<td class="col-md-2">4.45 - 5.30</td>
+								<td class="col-md-4">Pre-primary/Primary including Primary Exam Prep</td>
+                                <td class="col-md-3">Aldgate Oval Hall</td>
 							</tr>
 						</table>
 					</div>
