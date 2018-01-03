b308350b5227cccc8a39e1b8a89bee3b3ef1bb75
sberry <simon_berry2003@yahoo.com>
2018-01-03 20:40:48 +1030

    Added register page to alert

--- WebContent/alert.php
+++ WebContent/alert.php
@@ -1,4 +1,5 @@
 <div class="alert alert-danger" role="alert">
 	Our new blog is now available at <a href="http://www.hillsballetschool.edublogs.org" target="_TOP">www.hillsballetschool.edublogs.org</a>
+	<p>You can now enroll your dancer online <a href="contact.php" target="_TOP">here.</a>
 </div>
  
--- WebContent/register3.php
+++ WebContent/register3.php
@@ -2,14 +2,14 @@
     <div class="col-md-5">
         <div class="panel panel-default">
             <div class="panel-heading">
-                <h4><i class="fa fa-fw fa-check"></i>Register Your Interest</h4>
+                <h4><i class="fa fa-fw fa-check"></i>Enroll Your Dancer</h4>
             </div>
 			<div class="panel-body">
 				Please click
 				<a href="https://www.thinksmartsoftware-au.com/ocr/lesson_booking.php?t=XIFuy169oqznCgErV6pBeQ%3D%3D&u=jijlkOd9N87jHJmNzePMtw%3D%3D&p_id=1" target="_blank">
 					here
 				</a>
-				to register your dancer.
+				to enroll your dancer for 2018 classes.
 			</div>
 		</div>
     </div>
